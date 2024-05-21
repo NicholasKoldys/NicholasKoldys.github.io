21176c89e5af7b2e1dfb1d13579093c27909d170
NicholasKoldys <nicholas.koldys@gmail.com>
2024-05-21 13:56:55 +0700

    add base, dev, prod finals

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,135 +1,70 @@
-// import { merge } from "webpack-merge";
 import path from 'node:path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'css-minimizer-webpack-plugin';
-import { EsbuildPlugin } from 'esbuild-loader';
 import { commonConfig } from "./webpack.base.config.js";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import 'style-loader';
 
 process.env.NODE_ENV = 'development'
 
-export default  /* merge( commonConfig, { */
+export default
 { 
     ...commonConfig, 
     mode: "development",
-    // devtool: "inline-source-map",
     devtool: "eval-source-map", // ? Allows accurate debugging with source code in browser
+    
 
     entry: {
-        main: path.resolve("src/index.js"),
-        // vendor:["vue","vuex","./web/common/js/adapt",entryPath,'webpack/hot/dev-server'],
+        main: path.resolve("./src/index.js"),
     },
 
-/*     module: {
+    plugins: [
+      ...commonConfig.plugins, 
+      new HtmlWebpackPlugin({
+        template: path.resolve('./src/template.html'),
+        publicPath: commonConfig.output.publicPath,
+        filename: 'index.html',
+        scriptLoading: 'defer',
+        inject: true,
+      }),
+      new MiniCssExtractPlugin({ 
+        filename: '[name].css'
+      }),
+    ],
+
+    module: {
         rules: [
-            ...commonConfig.module.rules,
-            
+          ...commonConfig.module.rules,
+          { test: /\.(sc|sa|c)ss$/i, exclude: /\.module\.(sc|sa|c)ss$/, 
+              use: [
+                MiniCssExtractPlugin.loader, 
+                'css-loader',
+                { loader: 'postcss-loader', options: { postcssOptions: {
+                    plugins: [ 'autoprefixer', 'postcss-preset-env',  ]
+                } } }, 
+              ],
+              sideEffects: true,
+          },
+          { test: /\.module\.(sc|sa|c)ss$/i,
+              use: [
+                MiniCssExtractPlugin.loader, 
+                { loader: 'css-loader', 
+                  options: { 
+                    modules: "local",
+                    modules: {
+                      mode: "local",
+                      auto: true,
+                      namedExport: true,
+                      localIdentName: "[name]__[local]--[hash:4]",
+                      exportLocalsConvention: "camelCaseOnly",
+                    },
+                    importLoaders: 1, 
+                } },
+                { loader: 'postcss-loader', 
+                  options: { postcssOptions: {
+                    plugins: [ 'autoprefixer', 'postcss-preset-env',  ]
+                } } }, 
+              ],
+          },
         ]
-    } */
-
-    // plugins: [
-    //     new webpack.ProvidePlugin({
-    //         Vue:"vue",
-    //         Vuex:"vuex",
-    //         _$:require.resolve("./web/common/js/common.js"),
-    //         _net:require.resolve("./web/common/js/net"),
-    //         _session:require.resolve("./web/common/js/local"),
-    //         $:require.resolve("./web/console/common/js/common.js"),
-    //         net:require.resolve("./web/console/common/js/net.js"),
-    //         session:require.resolve("./web/console/common/js/local"),
-    //         helper:require.resolve("./web/console/common/js/helper"),
-    //     }),
-    //     new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: '[name].bundle.js'}),
-    //     //new BundleAnalyzerPlugin()
-    //     new webpack.NamedModulesPlugin(),
-    //     new webpack.HotModuleReplacementPlugin()
-    // ],
-
-    // module: {
-    //     loaders: [
-    //         {
-    //             test: /\.css$/,
-    //             loaders: ['style', 'css'],
-    //             include: path.resolve(__dirname, 'web')
-    //         },
-    //         {
-    //             test: /\.vue$/,
-    //             loader:"vue-loader?cacheDirectory",
-    //             include: path.resolve(__dirname, 'web'),
-    //             options: {
-    //                 loaders: {
-    //                     js: 'babel-loader'
-    //                 }
-    //             }
-    //         },
-    //         {
-    //             test: /\.html$/,
-    //             loader: "html-loader?attrs=img:src img:data-src",
-    //             include: path.resolve(__dirname, 'web')
-    //         },
-    //         {
-    //             test: /\.(png|jpg|gif|jpeg)$/,
-    //             loader: "url-loader?limit=30000",
-    //             include: path.resolve(__dirname, 'web')
-    //         }
-    //     ]
-    // },
-
-    // target: "electron-renderer",
-    // devtool: '#cheap-module-eval-source-map',
-    // devtool: 'source-map',
-
-    // resolve: {
-    //     modules: [path.resolve(__dirname, 'web/console'),path.resolve(__dirname, 'node_modules')],
-    //     alias: {
-    //         "vue": path.join(__dirname, 'node_modules/vue/dist/vue.min'),
-    //         "vuex": path.join(__dirname, 'node_modules/vuex/dist/vuex.min')
-    //     }
-    // },
-
-    // resolveLoader: {
-    //     modules: [path.resolve(__dirname, 'node_modules')],
-    // },
-
-    // externals: [nodeExternals()],
-    
-    // devServer: {
-        // files are served from this folder
-        // contentBase: 'dist',
-        // support HTML5 History API for react router
-        // historyApiFallback: true,
-        // listen to port 5000, change this to another port if another server 
-        // is already listening on this port
-        // contentBase: path.join( __dirname, 'public'),
-        // compress: true, 
-        // port: 5000, 
-        // static: './public'
-        /* allowedHosts?,  
-        bonjour?, 
-        client?, 
-        compress?, 
-        devMiddleware?, 
-        headers?, 
-        historyApiFallback?, 
-        host?, 
-        hot?, 
-        ipc?, 
-        liveReload?, 
-        onListening?, 
-        open?, 
-        port?, 
-        server?, 
-        setupExitSignals?, 
-        setupMiddlewares?, 
-        static?, 
-        watchFiles?, 
-        webSocketServer?  */
-
-        // proxy requests to the JSON server REST service
-        /* proxy: {
-            '/widgets': {
-                // server to proxy
-                target: 'http://0.0.0.0:3010'
-            }
-        } */
-    // }
+    }
 };
