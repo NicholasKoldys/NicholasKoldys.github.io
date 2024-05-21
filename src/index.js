ddbc37aaae48df0e76babf54d1880ee2bfe94425
NicholasKoldys <nicholas.koldys@gmail.com>
2024-05-21 14:44:40 +0700

    add html-script parse for web componments; remove unused imports;

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import "../public/style/globals.css";
-// import "../public/style/main.sass";
-import "../public/style/light.css";
+import "./public/style/colors.css";
+import "./public/style/global.css";
 
-import "../public/js/enivronment-variables.js";
-// import "../public/js/temp-debug.js";
-import "../public/js/element-dimensions-utilities.js";
-import "../public/js/inview-port-utilities.js";
-import "../public/js/dynamic-content.js";
-import "../public/js/button-event-assignments.js";
-import "../public/js/animation.js";
-import "../public/js/body-init.js";
+( () => {
+    window.addEventListener('DOMContentLoaded', (event) => {
+        const allElements = Array.from(document.querySelectorAll('*'));
+        allElements
+            .filter((el, i)  => el.tagName.includes('-') && allElements.indexOf(el) === i)
+            .map( el => {
+
+                let tag = el.tagName.toLowerCase();
+
+                switch (true) {
+                    case tag.includes('section'):
+                        import(`./pages/sections/${tag}.js`);
+                        break;
+                    case tag.includes('page'):
+                        import(`./pages/${tag}.js`);
+                        break;
+                    default:
+                        import(`./components/${tag}.js`);
+                        break;
+                }
+            } );
+    });
+})();
+
+import "./public/style/finalize.css";
