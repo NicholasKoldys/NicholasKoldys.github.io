c2e7944e98a58a568667baa92cf381c85493d572
NicholasKoldys <nicholas.koldys@gmail.com>
2024-05-22 11:56:16 +0700

    rename index run-once function to give more info

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import "./public/style/colors.css";
 import "./public/style/global.css";
 
-( () => {
+( function loadWebComponents() {
     window.addEventListener('DOMContentLoaded', (event) => {
         const allElements = Array.from(document.querySelectorAll('*'));
         allElements
-            .filter((el, i)  => el.tagName.includes('-') && allElements.indexOf(el) === i)
-            .map( el => {
+        .filter((el, i)  => el.tagName.includes('-') && allElements.indexOf(el) === i)
+        .map( el => {
 
-                let tag = el.tagName.toLowerCase();
+            let tag = el.tagName.toLowerCase();
 
-                switch (true) {
-                    case tag.includes('section'):
-                        import(`./pages/sections/${tag}.js`);
-                        break;
-                    case tag.includes('page'):
-                        import(`./pages/${tag}.js`);
-                        break;
-                    default:
-                        import(`./components/${tag}.js`);
-                        break;
-                }
-            } );
+            switch (true) {
+                case tag.includes('section'):
+                    import(`./pages/sections/${tag}.js`);
+                    break;
+                case tag.includes('page'):
+                    import(`./pages/${tag}.js`);
+                    break;
+                default:
+                    import(`./components/${tag}.js`);
+                    break;
+            }
+        } );
     });
 })();
 
